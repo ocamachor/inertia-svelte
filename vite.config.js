c5a49d0fede47d1e067a57b68770f6433abdb810
Orlando Camacho <ocamachor@gmail.com>
2023-09-06 09:45:10 -0500

    correcciones finales

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+	plugins: [
+		laravel({
+			input: ['resources/css/app.css', 'resources/js/app.js'],
+			refresh: true,
+		}),
+		svelte({
+			/* plugin options */
+		})
+	],
 });
